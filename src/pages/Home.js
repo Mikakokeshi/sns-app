101c9606fe259b98527eb42898141255b16ea559
Mikakokeshi <mikakokeshi00124@gmail.com>
2024-05-30 17:18:28 +0900

    ✨ Added feature of posting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Navigate } from 'react-router-dom'
+import { SessionContext } from '../SessionProvider'
+import { SideMenu } from '../components/SideMenu';
+import { postRepository } from '../repositories/post';
+import { Post } from '../components/Post';
+
+
 
 function Home() {
+    const {currentUser} = useContext(SessionContext);
+
+    const [content, setContent] = useState('')
+    const [posts, setPosts] = useState([])
+
+    useEffect(()=>{
+      fetchPosts();
+    },[])
+
+
+    const createPost = async () => {
+      const post = await postRepository.create(content, currentUser.id)
+      console.log('post',post);
+      setContent('');
+    }
+
+    const fetchPosts = async () => {
+      const posts = await postRepository.find();
+      setPosts(posts);
+    }
+
+    if(currentUser == null ) return <Navigate replace to ="/signin"/>;
   return (
-    <div>Home</div>
-  )
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-[#444] p-4">
+        <div className="container mx-auto flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-white">SNS APP</h1>
+          <button className="text-white hover:text-red-600">ログアウト</button>
+        </div>
+      </header>
+      <div className="container mx-auto mt-6 p-4">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-2">
+            <div className="bg-white p-4 rounded-lg shadow-md">
+              <textarea
+                className="w-full p-2 mb-4 border-2 border-gray-200 rounded-md"
+                placeholder="What's on your mind?"
+                onChange={(e)=> setContent(e.target.value)}
+                value={content}
+              />
+              <button 
+              onClick={createPost}
+              disabled={content === '' }
+              className="bg-[#444] text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed">
+                Post
+              </button>
+            </div>
+            <div className="mt-4">
+              {posts.map((post) => 
+                (<Post key={post.id} post={post}/>))}
+            </div>
+          </div>
+        <SideMenu />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
+
 
