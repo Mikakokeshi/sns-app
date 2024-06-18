7d1bd58302ad3a4daee186a17efbf23d0df49a40
Mikakokeshi <mikakokeshi00124@gmail.com>
2024-06-18 18:30:16 +0900

    Deleted unnecessary code

--- src/components/ModalArea.js
+++ src/components/ModalArea.js
@@ -1,45 +1,44 @@
-import React, { useState ,useContext} from 'react'
+import React, { useState, useContext } from "react";
 import Modal from "react-modal";
-import { postRepository } from '../repositories/post';
+import { postRepository } from "../repositories/post";
 import { supabase } from "../lib/supabase";
-import { SessionContext } from '../SessionProvider'
-
+import { SessionContext } from "../SessionProvider";
 
 export const ModalArea = (props) => {
-  const {currentUser, setCurrentUser} = useContext(SessionContext);
-
+  const { currentUser, setCurrentUser } = useContext(SessionContext);
 
   const [content, setContent] = useState("");
   const [id, setId] = useState();
-    
-  // async function updatePost() {
-  //       await postRepository.update({
-  //     content: title,
-  //  }).eq('id', id);
-  // }
 
-const userId =  currentUser.id
-
-const updatePost = async()=> {
+  const updatePost = async () => {
     try {
-        const { data, error } = await supabase
-            .from("posts")
-            .update({
-                content: content,
-                created_at: new Date()                
-            })
-            .eq("id", id)
+      const { data, error } = await supabase
+        .from("posts")
+        .update({
+          content: content,
+          created_at: new Date(),
+        })
+        .eq("id", id);
 
-            if (error) throw error;
-            window.location.reload();
-
+      if (error) throw error;
+      window.location.reload();
     } catch (error) {
-        alert(error.message);
+      alert(error.message);
     }
-    // props.setPosts([{...props.post, content:title, user_id: currentUser.id, user_name: currentUser.userName, email:currentUser.email  }]);
-    props.setPosts([{...props.post, id: id, content: content, user_id: currentUser.id, user_name: currentUser.userName, email:currentUser.email, created_at: new Date() }, ...props.posts]);
-    setContent('');
-}
+    props.setPosts([
+      {
+        ...props.post,
+        id: id,
+        content: content,
+        user_id: currentUser.id,
+        user_name: currentUser.userName,
+        email: currentUser.email,
+        created_at: new Date(),
+      },
+      ...props.posts,
+    ]);
+    setContent("");
+  };
 
   const customStyles = {
     content: {
@@ -60,51 +59,51 @@ const updatePost = async()=> {
 
   async function updateFunc() {
     await updatePost();
-    // closeModal();
-  
   }
-
-
 
   const editContent = (e, id) => {
     setContent(e);
     setId(id);
-  console.log(content, id, props.post.content, props.getSelectedContent.content)
-  }
+    console.log(
+      content,
+      id,
+      props.post.content,
+      props.getSelectedContent.content
+    );
+  };
 
-
-  if(!props.selectedContent){
-    return ;
+  if (!props.selectedContent) {
+    return;
   }
 
   return (
     <>
-    <Modal 
-      isOpen={props.editModalIsOpen} 
-      style={customStyles} 
-      onRequestClose={closeModal}
-    >
-      <h3 className="text-lg font-semibold border-b-2 pb-2 mb-4">編集画面</h3>
-      <input 
-        type='text' 
-        className="text-gray-700 text-xl" 
-        defaultValue={props.getSelectedContent.content} 
-        onChange={
-          (e)=>editContent(e.target.value, props.getSelectedContent.id)
-        }
-      />
+      <Modal
+        isOpen={props.editModalIsOpen}
+        style={customStyles}
+        onRequestClose={closeModal}
+      >
+        <h3 className="text-lg font-semibold border-b-2 pb-2 mb-4">編集画面</h3>
+        <input
+          type="text"
+          className="text-gray-700 text-xl"
+          defaultValue={props.getSelectedContent.content}
+          onChange={(e) =>
+            editContent(e.target.value, props.getSelectedContent.id)
+          }
+        />
 
-    <div className="pt-2 text-right">
-        <button 
+        <div className="pt-2 text-right">
+          <button
             onClick={() => {
               updateFunc();
             }}
-            className="text-blue-500 hover:underline cursor-pointer focus:outline-none">
-          保存
-        </button> 
-      </div>
-  </Modal>
-</>
-  )
-  
-}
+            className="text-blue-500 hover:underline cursor-pointer focus:outline-none"
+          >
+            保存
+          </button>
+        </div>
+      </Modal>
+    </>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,110 +1,91 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Navigate } from 'react-router-dom'
-import { SessionContext } from '../SessionProvider'
-import { SideMenu } from '../components/SideMenu';
-import { postRepository } from '../repositories/post';
-import { Post } from '../components/Post';
-import { Pagination } from '../components/Pagination';
-import { authRepository } from '../repositories/auth';
-import { ModalArea } from '../components/ModalArea';
-import { supabase } from "../lib/supabase";
+import React, { useContext, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { SessionContext } from "../SessionProvider";
+import { SideMenu } from "../components/SideMenu";
+import { postRepository } from "../repositories/post";
+import { Post } from "../components/Post";
+import { Pagination } from "../components/Pagination";
+import { authRepository } from "../repositories/auth";
+import { ModalArea } from "../components/ModalArea";
 
 const limit = 5;
 
 function Home() {
-  
-    const {currentUser, setCurrentUser} = useContext(SessionContext);
+  const { currentUser, setCurrentUser } = useContext(SessionContext);
 
-    const [content, setContent] = useState('');
-    const [posts, setPosts] = useState([]);
-    const [page, setPage] = useState(1);
-    const [email, setEmail] = useState("");
-    const [selectedContent, setSelectedContent] = useState(false);
-    const [editModalIsOpen, setEditModalIsOpen] = useState(false);
+  const [content, setContent] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [selectedContent, setSelectedContent] = useState(false);
+  const [editModalIsOpen, setEditModalIsOpen] = useState(false);
 
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-    useEffect(()=>{
-      // window.location.reload();
-      fetchPosts();
+  const createPost = async () => {
+    const post = await postRepository.create(
+      content,
+      currentUser.id,
+      currentUser.email,
+      currentUser.userName
+    );
 
-    },[])
+    setPosts([
+      {
+        ...post,
+        user_id: currentUser.id,
+        user_name: currentUser.userName,
+        email: currentUser.email,
+      },
+      ...posts,
+    ]);
+    setContent("");
+  };
 
+  const fetchPosts = async (page) => {
+    const posts = await postRepository.find(page, limit, currentUser.userName);
+    setPosts(posts);
 
-    const createPost = async () => {
+    console.log(posts);
+  };
 
-      const post = await postRepository.create(content, currentUser.id, currentUser.email, currentUser.userName)
-      
-      setPosts([{...post, user_id: currentUser.id, user_name: currentUser.userName, email:currentUser.email  }, ...posts]);
-      setContent('');
-    }
+  const moveToNext = async () => {
+    const nextPage = page + 1;
+    await fetchPosts(nextPage);
+    setPage(nextPage);
+  };
 
-    const fetchPosts = async (page) => {
-      const posts = await postRepository.find(page, limit, currentUser.userName);
-        setPosts(posts);
+  const moveToPrev = async () => {
+    const prevPage = page - 1;
+    await fetchPosts(prevPage);
+    setPage(prevPage);
+  };
 
-        console.log(posts)
-    }
+  const deletePost = async (postId) => {
+    await postRepository.delete(postId);
+    setPosts(posts.filter((post) => post.id !== postId));
+  };
 
-    // const fetchPosts = async () => {
-    //   try {
-    //     const { data, error } = await supabase
-    //       .from("posts_view")
-    //       .select("*")
-    //       .limit(10)
-    //     if (error) throw error;
-        
-    //     if (data != null) {
-    //       setPosts(data); // [product1,product2,product3]
-    //       return data.map((post) => {
-    //         return {
-    //             ...post,
-    //             userId : post.user_id,
-    //             userName : post.user_metadata.name,
-    //         };
-    //     });
-    //     }
-    //   } catch (error) {
-    //     alert(error.message);
-    //   }
+  const getSelectedContent = () => {
+    return posts.find((post) => post.id === selectedContent);
+  };
 
-    // }
+  const signout = async () => {
+    await authRepository.signout();
+    setCurrentUser(null);
+  };
 
-    const moveToNext = async()=> {
-      const nextPage = page + 1;
-      await fetchPosts(nextPage);
-      setPage(nextPage);
-    }
-
-    const moveToPrev = async()=> {
-      const prevPage = page - 1;
-      await fetchPosts(prevPage);
-      setPage(prevPage);
-    }
-  
-    const deletePost = async(postId) => {
-      await postRepository.delete(postId);
-      setPosts(posts.filter((post) => post.id !== postId))
-    }
-
-
-    const getSelectedContent = () => {
-      return posts.find((post) => post.id === selectedContent);
-    }
-  
-
-    const signout = async() => {
-      await authRepository.signout();
-      setCurrentUser(null);
-    }
-
-    if(currentUser == null ) return <Navigate replace to ="/signin"/>;
+  if (currentUser == null) return <Navigate replace to="/signin" />;
 
   return (
     <div className="min-h-screen bg-gray-100">
       <header className="bg-[#444] p-4">
         <div className="container mx-auto flex items-center justify-between">
           <h1 className="text-3xl font-bold text-white">SNS APP</h1>
-          <button className="text-white hover:text-red-600" onClick={signout}>ログアウト</button>
+          <button className="text-white hover:text-red-600" onClick={signout}>
+            ログアウト
+          </button>
         </div>
       </header>
       <div className="container mx-auto mt-6 p-4">
@@ -114,51 +95,46 @@ function Home() {
               <textarea
                 className="w-full p-2 mb-4 border-2 border-gray-200 rounded-md"
                 placeholder="What's on your mind?"
-                onChange={(e)=> setContent(e.target.value)}
+                onChange={(e) => setContent(e.target.value)}
                 value={content}
               />
-              <button 
-              onClick={createPost}
-              disabled={content === '' }
-              className="bg-[#444] text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed">
+              <button
+                onClick={createPost}
+                disabled={content === ""}
+                className="bg-[#444] text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+              >
                 Post
               </button>
             </div>
             <div className="mt-4">
-              {posts.map((post) => 
-                (
-                  <div key={post.id} >
-                <Post 
-                  post={post} 
-                  onDelete={deletePost} 
-                  selectedContent={selectedContent} 
-                  setSelectedContent={setSelectedContent} 
-                  setEditModalIsOpen={setEditModalIsOpen}
-                  setEmail={setEmail}
+              {posts.map((post) => (
+                <div key={post.id}>
+                  <Post
+                    post={post}
+                    onDelete={deletePost}
+                    setSelectedContent={setSelectedContent}
+                    setEditModalIsOpen={setEditModalIsOpen}
                   />
-                  
-                  <ModalArea 
-                  posts={posts}
-                    post={post} 
+
+                  <ModalArea
+                    posts={posts}
+                    post={post}
                     setPosts={setPosts}
                     setEditModalIsOpen={setEditModalIsOpen}
                     editModalIsOpen={editModalIsOpen}
-                    selectedContent={selectedContent} 
-                    // setSelectedContent={setSelectedContent} 
-                    getSelectedContent={getSelectedContent()} 
-                    // updatePost={updatePost}
-                    // updatedContent={updatedContent}
-                    setContent={setContent}
-                    content={content}
+                    selectedContent={selectedContent}
+                    getSelectedContent={getSelectedContent()}
                   />
-                  </div>
-
-                  ))}
+                </div>
+              ))}
             </div>
 
-            <Pagination onPrev={ page > 1 ? moveToPrev: null} onNext={posts.length >= limit ? moveToNext : null} />
+            <Pagination
+              onPrev={page > 1 ? moveToPrev : null}
+              onNext={posts.length >= limit ? moveToNext : null}
+            />
           </div>
-        <SideMenu />
+          <SideMenu />
         </div>
       </div>
     </div>
@@ -166,5 +142,3 @@ function Home() {
 }
 
 export default Home;
-
-
